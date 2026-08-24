ef7bc6217131a8bcaf6a1d1caae18417585a1ceb
bioshock <8wf96xysqt@privaterelay.appleid.com>
2026-08-24 16:44:07 +0900

    PushEndo

---
+++ GameJamSummer/Assets/Endo/scripts/FireBall.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    [Header("移動速度")]
+    [SerializeField]
+    private float MoveSpeed = 5.0f;
+
+    [Header("バウンド可能回数")]
+    [SerializeField]
+    private int BoundCount = 3;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        // 初期方向へ移動
+        rb.linearVelocity = transform.forward * MoveSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Wall"))
+        {
+            return;
+        }
+
+        Debug.Log($"Wall衝突！ BoundCount = {BoundCount}");
+
+        if (BoundCount <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        BoundCount--;
+
+        Vector3 direction = Vector3.Reflect(
+            rb.linearVelocity.normalized,
+            collision.contacts[0].normal
+        );
+
+        rb.linearVelocity = direction * MoveSpeed;
+    }
+
+}
