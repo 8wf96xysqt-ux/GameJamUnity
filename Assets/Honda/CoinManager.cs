4cc1edd90da4e542360bd633b5ac4a2403c2ba4f
owataomaemona <bentianowata@gmail.com>
2026-08-26 12:10:03 +0900

    コインが出来ました

---
+++ Assets/Honda/CoinManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CoinManager : MonoBehaviour
+{
+    private List<Vector3> coinPositions = new List<Vector3>();
+
+    public GameObject coinPrefab;
+
+    void Start()
+    {
+        GameObject[] coinpoints = GameObject.FindGameObjectsWithTag("CoinPoint");
+
+        foreach (GameObject coinpoint in coinpoints)
+        {
+            coinPositions.Add(coinpoint.transform.position);
+        }
+
+        GameObject coin = Instantiate(coinPrefab);
+
+        coin.transform.position = coinPositions[0];
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
