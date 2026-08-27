f208b5f4f2d8f8e5e132a6d060c9fd1fda7d101f
owataomaemona <bentianowata@gmail.com>
2026-08-27 12:04:39 +0900

    コインのスクリプトを変更しました

--- GameJamSummer/Assets/Honda/Coin/Script/Coin.cs
+++ GameJamSummer/Assets/Honda/Coin/Script/Coin.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
+using TMPro;
 
 // コインの回転と取得処理と取得した際のコインの獲得数を増やすスクリプト
 
 public class Coin : MonoBehaviour
 {
-    private int countCoin;
+    private int countCoin = 0;
+
+    [HideInInspector]
+    public TextMeshProUGUI countCoinText;
+
+    private void Start()
+    {
+        if (countCoinText == null)
+        {
+            countCoinText = GameObject.Find("Text (TMP)").GetComponent<TextMeshProUGUI>(); // TextMeshProUGUIコンポーネントを取得
+        }
+    }
 
     private void Update()
     {
@@ -15,9 +27,16 @@ public class Coin : MonoBehaviour
     {
         if (collison.CompareTag("Eraser"))
         {
-            Destroy(gameObject); // コインを消す
-            
+            if (countCoin != 0)
+            {
+                countCoinText.text = "×" + countCoin.ToString();
+            }
+
+            Destroy(gameObject); // コインを消す 
+
             countCoin++; // 取得したコインの数を増やす
+
         }
     }
+
 }
